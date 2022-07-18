7e5c84f451e7bec2874b52165276ec2176560e5e
Bence Patyi <bence.patyi@lhsystems.com>
2022-07-18 17:00:59 +0200

    add: validation method to Todo model

--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,18 @@
 package models
 
+import validation "github.com/go-ozzo/ozzo-validation/v4"
+
 type Todo struct {
 	ID          string
 	Title       string
 	Description string
+}
+
+func (t Todo) Validate() error {
+	return validation.ValidateStruct(
+		&t,
+		validation.Field(&t.ID, validation.Required),
+		validation.Field(&t.Title, validation.Required),
+		validation.Field(&t.Description, validation.Required),
+	)
 }
